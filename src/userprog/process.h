cdfb10bc141fac28569a67bd95c4596d36ce2c9c
yelun <ye.yang20@imperial.ac.uk>
2021-11-05 09:31:47 +0000

    feat: argument passing and stack setup

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -8,4 +8,14 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
+struct argument {
+  struct list_elem elem; /* List element for argv list in struct setup_data */
+  char *arg;             /* string argument */
+  void *stack_addr;      /* memory of where the argument is stored for stack */
+}
+
+struct setup_data {
+  struct list argv;       /* list of arguments */
+  int argc;               /* number of arguments */
+}
 #endif /* userprog/process.h */
