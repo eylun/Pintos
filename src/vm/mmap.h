0719fadf5d97ecef54b8917c976d6a02f8aa2e54
wsl20 <wsl20@ic.ac.uk>
2021-12-03 23:21:05 +0000

    feat: added struct mmap_entry and mmap controller function signatures

--- src/vm/mmap.h
+++ src/vm/mmap.h
@@ -22,5 +22,3 @@ struct mmap_entry *mmap_search_mapping(struct hash *mmap_table, mapid_t mapid);
 void mmap_insert_mapping(struct mmap_entry *);
 void mmap_destroy_complete(void);
 void mmap_write_back_data(struct mmap_entry *entry, void *src, size_t offset, size_t length);
-
-#endif
